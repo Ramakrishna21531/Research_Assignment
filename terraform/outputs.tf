f8fd8824370c33d1205bc01cc5274ef90ca29006
Rama Krishna <ramakrishna531@gmail.com>
2026-05-06 12:41:24 -0400

    updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,12 +17,3 @@ output "ecr_frontend" {
 output "ecs_cluster" {
   value = aws_ecs_cluster.main.name
 }
-
-output "s3_bucket" {
-  value = aws_s3_bucket.data.bucket
-}
-
-output "processor_task_definition" {
-  description = "Use this in CI/CD to run the processor as a one-off task"
-  value       = aws_ecs_task_definition.processor.family
-}
